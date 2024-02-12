cdd51f8c15b74a829fb7e1b81b19b4707de2b046
Bastien <bastien.gauthier@ntymail.com>
2024-02-12 19:29:08 +0100

    Changement de version du schéma (2.3.1)

--- app/utils.py
+++ app/utils.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 
 URL_STABLE_DATAGOUV = (
-    "https://www.data.gouv.fr/fr/datasets/r/8d9398ae-3037-48b2-be19-412c24561fbb"
+    "https://www.data.gouv.fr/fr/datasets/r/eb76d20a-8501-400e-b336-d85724de5435"
 )
 
 APP_FOLDER = str(Path(__file__).parent.parent)
